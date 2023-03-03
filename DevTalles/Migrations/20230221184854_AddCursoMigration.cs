231c20e12305c18ed24d3fb271763066eec83aa0
CarlosDMB <medinadanielito5@gmail.com>
2023-03-02 21:23:10 -0600

    Agregar archivos de proyecto.

---
+++ DevTalles/Migrations/20230221184854_AddCursoMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevTalles.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCursoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreProducto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescripcionCorta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DescripcionProducto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Precio = table.Column<double>(type: "float", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false),
+                    SubCategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cursos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Cursos_SubCategorias_SubCategoriaId",
+                        column: x => x.SubCategoriaId,
+                        principalTable: "SubCategorias",
+                        principalColumn: "SubCategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cursos_CategoriaId",
+                table: "Cursos",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cursos_SubCategoriaId",
+                table: "Cursos",
+                column: "SubCategoriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cursos");
+        }
+    }
+}
---
+++ DevTalles/Models/SubCategoria.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevTalles.Models
+{
+    public class SubCategoria
+    {
+        [Key]
+        public int SubCategoriaId { get; set; }
+
+        [Required(ErrorMessage = "El Nombre de la Sub Categoria es obligatorio.")]
+        [Display(Name ="SubCategoria")]
+        [Remote(action:"ValidarNombre",controller:"SubCategoria")]
+        public string Nombre { get; set; }
+    }
+}
